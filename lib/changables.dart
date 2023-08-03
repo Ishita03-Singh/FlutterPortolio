ae785d858cacbb6746b55502c88859c4d5c57ebd
Ishita Singh <76431570+Ishita03-Singh@users.noreply.github.com>
2023-08-03 12:23:09 +0530

    Delete lib directory

--- lib/changables.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class Changables with ChangeNotifier {
-  ValueNotifier<int> changableCount = ValueNotifier<int>(0);
-}
-
-Changables changables = Changables();
